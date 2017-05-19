0199c421a2bb68bf9e18e87cdb2e417082f2eb48
Tim McIver <tim.mciver@maxwellhealth.com>
2017-05-19 13:43:21 -0400

    Updates.

--- examples/example.scala
+++ examples/example.scala
@@ -7,7 +7,14 @@ trait X {
 object X {
   private case class XImpl(a: Int, b: String) extends X {}
 
+  def unapply(x: XImpl): Option[(Int, String)] = Some((x.a, x.b))
   def mkX(a: Int, b: String): X = XImpl(a, b)
   def editA(x: X, newA: Int): X = XImpl(newA, x.b)
 }
 
+val x = X.mkX(1, "Hi")
+val y = X.mkX(1, "Hi")
+val z = X.mkX(2, "Hi")
+
+println("x == y: " + (x == y))
+println("x == z: " + (x == z))
